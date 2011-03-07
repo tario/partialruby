74d267a23e05562d8c86797ac7ebd4d54cbafd36
Dario <darios3@gmail.com>
2011-03-07 18:54:51 -0300

    added two failing tests to the spec

--- spec/eval_spec.rb
+++ spec/eval_spec.rb
@@ -6,4 +6,13 @@ describe Context, "PartialRuby context" do
 	it "should eval hello world string" do
 		PartialRuby.eval 'print "hello world\n"', binding
 	end
+
+  it "should define a class" do
+    PartialRuby.eval 'class X; end', binding
+  end
+
+  it "should define a class with a method" do
+    PartialRuby.eval 'class X; def foo; end; end', binding
+  end
+
 end
