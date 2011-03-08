7d50e6514b4cc3f64fd9ed584cd3f36533351f67
Dario <darios3@gmail.com>
2011-03-08 19:06:12 -0300

    added spec for flow control

---
+++ spec/flow_control_spec.rb
@@ -0,0 +1,18 @@
+require "partialruby"
+
+include PartialRuby
+
+describe Context, "PartialRuby context" do
+
+  def self.assert_ruby_expr(expr, expected = nil)
+
+    expected ||= eval(expr)
+
+    it "should return #{expected} on expresion #{expr}" do
+      PartialRuby.eval(expr,binding).should be == expected
+    end
+  end
+
+  assert_ruby_expr "if true; true; else; false; end"
+
+end
