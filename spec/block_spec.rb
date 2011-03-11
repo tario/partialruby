24d37f10f803a8ff958f9478824385ce7cee6470
Dario <darios3@gmail.com>
2011-03-10 21:32:22 -0300

    changed block test to make it more simple

--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -18,15 +18,14 @@ describe Context, "PartialRuby context" do
     PartialRuby.eval("BlockTest.foo{ BlockTest.bar }", binding)
   end
 
-  def self.test_block_arguments(args, args_result)
+  def self.test_block_argument(args, args_result)
     it "should pass block statements with arguments #{args_result}" do
       BlockTest.should_receive(:bar).with(*args_result)
-      PartialRuby.eval("BlockTest.foo(#{args}) { |x,y| BlockTest.bar }", binding)
+      PartialRuby.eval("BlockTest.foo(#{args}) { |x| BlockTest.bar(x) }", binding)
     end
   end
 
-  test_block_arguments "1", [1]
-  test_block_arguments "1,2", [1,2]
-  test_block_arguments "'test'", ["test"]
+  test_block_argument "1", [1]
+  test_block_argument "'test'", ["test"]
 
 end
