222b6e95e89be105a9e31ebb63872c3fa43b55d4
Dario <darios3@gmail.com>
2011-03-22 23:33:53 -0300

    fixed exception spec

--- spec/exception_spec.rb
+++ spec/exception_spec.rb
@@ -6,21 +6,26 @@ describe Context, "PartialRuby context" do
 
   def self.assert_ruby_expr(expr, expected = nil)
 
-    expected ||= eval(expr)
+    exception = nil
 
-    it "should return #{expected} on expresion #{expr}" do
-      PartialRuby.eval(expr,binding).should be == expected
+    begin
+      expected ||= eval(expr)
+    rescue Exception => e
+      exception = e
+    end
+
+    if exception
+      it "should raise #{exception} on expresion #{expr}" do
+        lambda{
+          PartialRuby.eval(expr,binding)
+        }.should raise_error(exception.class)
+      end
+    else
+      it "should return #{expected} on expresion #{expr}" do
+        PartialRuby.eval(expr,binding).should be == expected
+      end
     end
   end
 
-  def self.test_operators(*ops)
-    ops.each do |op|
-      assert_ruby_expr "2.0#{op}3.0"
-    end
-  end
-
-  test_operators "+", "-", "/", "*", "**"
-
-  assert_ruby_expr "[1][0]"
-
+  assert_ruby_expr("raise Exception")
 end
