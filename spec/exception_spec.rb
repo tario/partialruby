ab627c8b1ce73a9616534df946c7830ea60a8c4c
Dario <darios3@gmail.com>
2011-03-24 00:37:41 -0300

    added 37 exception tests (22 failing test)

--- spec/exception_spec.rb
+++ spec/exception_spec.rb
@@ -31,4 +31,26 @@ describe Context, "PartialRuby context" do
   assert_ruby_expr("begin; raise Exception; rescue; 5; end")
   assert_ruby_expr("begin; raise Exception; rescue Exception; 4; end")
   assert_ruby_expr("begin; raise Exception; rescue Exception => e; 9; ensure; 4; end")
+  assert_ruby_expr("begin; raise '999'; rescue RuntimeError; end")
+  assert_ruby_expr("begin; raise '999'; rescue RuntimeError; end; 5")
+  assert_ruby_expr("begin; raise '999'; rescue RuntimeError, Exception; end")
+  assert_ruby_expr("begin; raise '999'; rescue RuntimeError, Exception; end; 5")
+
+  [
+   "rescue RuntimeError; end",
+   "rescue RuntimeError => e; end",
+   "rescue Errno::EINVAL; end",
+   "rescue Errno::EINVAL => e; end",
+   "rescue RuntimeError, Errno::EINVAL; end",
+   "rescue RuntimeError => e, Errno::EINVAL; e; end",
+   "rescue RuntimeError, Errno::EINVAL => e; e; end",
+   "rescue RuntimeError => e, Errno::EINVAL => e2; e; end",
+   "rescue RuntimeError => e, Errno::EINVAL => e2; e2; end",
+  ].each do |x|
+  assert_ruby_expr("begin; raise Errno::EINVAL; #{x}")
+  assert_ruby_expr("begin; raise RuntimeError; #{x}")
+  assert_ruby_expr("begin; raise Errno::EINVAL; #{x}")
+  assert_ruby_expr("begin; raise RuntimeError; #{x}")
+  end
+
 end
