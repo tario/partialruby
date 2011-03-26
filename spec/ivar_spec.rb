12ea4f9233a6761737e84bbd0def031b00d817ff
Dario <darios3@gmail.com>
2011-03-26 16:31:40 -0300

    created spec for ivar nodes (+2 failing tests)

---
+++ spec/ivar_spec.rb
@@ -0,0 +1,18 @@
+require "partialruby"
+
+include PartialRuby
+
+describe Context, "PartialRuby context" do
+
+  def self.assert_ruby_expr(expr, expected = nil)
+
+    expected ||= eval(expr)
+
+    it "should return #{expected} on expresion #{expr}" do
+      PartialRuby.eval(expr,binding).should be == expected
+    end
+  end
+
+  assert_ruby_expr "@b"
+  assert_ruby_expr "@a = 4; @a"
+end
